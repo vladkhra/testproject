3b3ee85cc1b33c8da234f4dc40a2373599507cbb
Vladimir Khramtsov <vladimir@khramtsov.ca>
2017-06-14 14:32:12 -0700

    first checkjin

---
+++ MvcSample/ContosoUniversityTests/Controllers/InstructorControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ContosoUniversity.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Controllers.Tests
+{
+    [TestClass()]
+    public class InstructorControllerTests
+    {
+        private InstructorController controller;
+        [TestInitialize]
+        public void InitializeTest()
+        {
+            controller = new InstructorController();
+        }
+
+        [TestMethod()]
+        public void DetailsTest()
+        {
+            var viewResult = controller.Details(10) as ViewResult;
+            var model = viewResult.Model as Instructor;
+            Assert.IsNotNull(model);
+        }
+    }
+}
